1926bc83b4d8f3b71f25ce046bd4cccd1f5e6c41
Skovand <Skovand@proton.me>
2025-03-26 02:51:59 +0300

    forgot comments

--- math/Fizz Buzz.py
+++ math/Fizz Buzz.py
@@ -1,9 +1,11 @@
 
-
+# SOLVED 3/26/2025
+# Time taken: 16 minutes 30 seconds 
 
 
 class Solution:
     def fizzBuzz(self, n: int) -> list[str]:
+        # BEHOLD THE POWER OF NESTED TERNARIES *malicious exultating emoji
         result = [("FizzBuzz" if i%3 == 0 and i%5 == 0 else ("Fizz" if i%3 == 0 else ("Buzz" if i%5 == 0 else str(i)))) for i in range(1,n+1)]
         return result
 
