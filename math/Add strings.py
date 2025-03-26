1235f47c1b397eac172e443ece96d190940b7ccd
Skovand <Skovand@proton.me>
2025-03-26 03:19:15 +0300

    Add strings

--- math/Add strings.py
+++ math/Add strings.py
@@ -1,11 +1,16 @@
 
+# SOLVED 3/26/2025
+# Time taken: 16:21 
+
+import sys 
+
+sys.set_int_max_str_digits(5000)    # to allow large digits conversion 
 
 
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        result = eval(f"hex({num1}) + hex({num2})")
-        result = str(result)
-        return eval("int({result}, 16)")
+        result = str(eval(f"{num1}+{num2}"))
+        return result 
 
 
 
