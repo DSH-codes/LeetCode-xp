9cf3aa40814c802fc5bef5546de1195fd90a5259
Skovand <Skovand@proton.me>
2025-05-06 02:41:44 +0300

    Student Attendance Record I

---
+++ string/Student Attendance Record I.py
@@ -0,0 +1,22 @@
+
+
+
+class Solution:
+    
+    def checkRecord(self, s: str) -> bool:
+
+        counter = 0
+        
+        if s.count("A") > 2:
+            return False
+
+        if "LLL" in s:
+            return False
+
+        return True
+
+it = Solution()
+
+result = it.checkRecord("LPPALL")
+
+print(result)
