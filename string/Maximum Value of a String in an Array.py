0d7154bf43ad8c897863a942866755fc8daae094
Skovand <Skovand@proton.me>
2025-05-21 02:00:03 +0300

    easy

---
+++ string/Maximum Value of a String in an Array.py
@@ -0,0 +1,15 @@
+
+
+
+
+class Solution:
+    def maximumValue(self, strs: list[str]) -> int:
+        
+        return max([int(i) if i.isdigit() else len(i) for i in strs])
+
+
+
+
+it = Solution()
+result = it.maximumValue(["alic3","bob","3","4","00000"])
+print(result)
