a79b68e0045fa3904e34f200958393c3ae19d68e
Skovand <Skovand@proton.me>
2025-05-21 00:38:01 +0300

    easy

--- string/Counting Words With a Given Prefix.py
+++ string/Counting Words With a Given Prefix.py
@@ -1,10 +1,17 @@
 
 
+# Solved in: 4 m 14 sec
+# 5/21/2025
+# Runtime: 0 ms - beats 100%
+# Memory 17.77 mb - 91.37% 
+
+
 
 class Solution:
     def prefixCount(self, words: list[str], pref: str) -> int:
         
-        # return len([i for i in words if i.startswith(pref)])
+        # return len([i for i in words if i.startswith(pref)]) - one liner! 
+
 
         count = 0
 
@@ -18,5 +25,5 @@ class Solution:
 
 
 it = Solution()
-result = it.prefixCount(["pay","attention","practice","attend"], "at")
+out = it.prefixCount(["pay","attention","practice","attend"], "at")
 print(result)
