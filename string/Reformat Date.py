13e6290e221a5e8c6d4f65a06c3669b81ba997f1
Skovand <Skovand@proton.me>
2025-06-02 19:25:05 +0300

    easy

---
+++ string/Reformat Date.py
@@ -0,0 +1,31 @@
+
+
+
+
+# https://leetcode.com/problems/reformat-date/?envType=problem-list-v2&envId=string
+# Solved in: 23 m 40 sec
+# 6/2/2025
+# Runtime: 0 ms - beats 100%
+# Memory: 17.55 mb - beats 97.40%
+
+
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        
+        months = [i for i in "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split()]
+        
+        sdate = date.split()
+
+        day = "".join([i for i in sdate[0] if i.isdigit()])
+        day = day if len(day) == 2 else "0" + day
+        
+        month = months.index(sdate[1]) + 1
+        month = month if len(str(month)) == 2 else "0" + str(month)
+
+        return f"{sdate[2]}-{month}-{day}"
+
+
+
+it = Solution()
+result = it.reformatDate("20th Oct 2052")
+print(result)
