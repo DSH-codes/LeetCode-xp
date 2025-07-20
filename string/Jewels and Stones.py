f8e1c867bb3c7206eed3108054815ce4cd5b6f9f
Skovand <Skovand@proton.me>
2025-07-20 14:08:54 +0300

    easy

---
+++ string/Jewels and Stones.py
@@ -0,0 +1,23 @@
+
+# https://leetcode.com/problems/jewels-and-stones/submissions/1704659753/?envType=problem-list-v2&envId=string
+# Solved in: 2 m 40 sec 
+# 7/20/2025
+# Runtime: 0 ms - beats 100%
+# Memory: 17.94 mb - beats 10.47% 
+
+
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        
+        return len([i for i in stones if i in jewels])
+
+
+
+
+it = Solution()
+result = it.numJewelsInStones("aA", "aAAbbbb")
+print(result)
+
+
+
